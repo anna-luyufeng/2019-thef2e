dd8ff2d6d054f9d1795a8e95882ecba41f49ae59
littlegreening <littlegreening@gmail.com>
2019-07-11 18:44:05 +0800

    ADD: settings of deploying to gh-page
    add deploy.sh
    change vue.config.js publicPath and outputDir
    update package.json build script

--- week1-pomodoro/vue.config.js
+++ week1-pomodoro/vue.config.js
@@ -1,23 +1,26 @@
 const path = require('path')
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '@': resolveSrc('.'),
-                '@src': resolveSrc('src'),
-                '@views': resolveSrc('src/views'),
-                '@layouts': resolveSrc('src/views/layouts'),
-                '@components': resolveSrc('src/components'),
-                '@design': resolveSrc('src/design/index.scss'),
-            }
-        }
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolveSrc('.'),
+        '@src': resolveSrc('src'),
+        '@views': resolveSrc('src/views'),
+        '@layouts': resolveSrc('src/views/layouts'),
+        '@components': resolveSrc('src/components'),
+        '@design': resolveSrc('src/design/index.scss'),
+      },
     },
-    css: {
-        // Enable CSS source maps.
-        sourceMap: true,
-    },
+  },
+  css: {
+    // Enable CSS source maps.
+    sourceMap: true,
+  },
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/2019-thef2e/week1/' : '/',
+  outputDir: '../public/week1',
 }
 
 function resolveSrc(_path) {
-    return path.resolve(__dirname, _path)
+  return path.resolve(__dirname, _path)
 }
