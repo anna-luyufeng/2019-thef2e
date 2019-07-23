9d697fa4e4f2446522b49089bbf266cb9e265dfc
littlegreening <littlegreening@gmail.com>
2019-07-23 19:35:32 +0800

    [Week 2] add basic deck with suffled cards

--- week2/src/App.vue
+++ week2/src/App.vue
@@ -1,28 +1,43 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Deck from "./deck";
+
+const deckColumns = 8;
 
 export default {
-  name: "app",
-  components: {
-    HelloWorld
+  name: "App",
+  components: {},
+  data() {
+    return {
+      columns: []
+    };
+  },
+  mounted() {
+    this.dealCardsToColumns();
+  },
+  methods: {
+    dealCardsToColumns() {
+      // TODO: need to copy this suffle result to restart the game
+      const getSuffledCards = Deck.shuffle();
+      const cardsPerColumn = Math.floor(getSuffledCards.length / deckColumns);
+      const columnsBonus = getSuffledCards.length % deckColumns;
+
+      for (let column = 1; column <= deckColumns; column++) {
+        const dealNumber = cardsPerColumn + (column <= columnsBonus ? 1 : 0);
+        this.columns.push(Deck.deal(dealNumber));
+      }
+    }
   }
 };
 </script>
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(column, index) in columns" :key="index">
+        <div v-for="card in column" :key="`${card.suit}${card.value}`">{{card.suit}}{{card.value}}</div>
+      </li>
+    </ul>
+  </div>
+</template>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ week2/src/deck.js
@@ -0,0 +1,49 @@
+class Card {
+  constructor(suit, value) {
+    this.suit = suit;
+    this.value = value;
+  }
+}
+
+class Deck {
+  constructor() {
+    this.deck = [];
+  }
+  // 按照順序製造的牌
+  createDeck(suits, values) {
+    for (let suit of suits) {
+      for (let value of values) {
+        this.deck.push(new Card(suit, value));
+      }
+    }
+  }
+  // 洗牌
+  shuffle() {
+    let counter = this.deck.length;
+    let randomIndex = "";
+    let cacheRandomCard = "";
+
+    while (counter) {
+      randomIndex = Math.floor(Math.random() * counter--);
+      cacheRandomCard = this.deck[counter];
+      this.deck[counter] = this.deck[randomIndex];
+      this.deck[randomIndex] = cacheRandomCard;
+    }
+    return this.deck;
+  }
+  // 發牌
+  deal(number) {
+    let hand = [];
+    while (hand.length < number) {
+      hand.push(this.deck.pop());
+    }
+    return hand;
+  }
+}
+
+const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
+
+const deck = new Deck();
+deck.createDeck(suits, values);
+export default deck;
